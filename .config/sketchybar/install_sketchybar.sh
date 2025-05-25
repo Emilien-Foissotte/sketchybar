f46b76078ab65069105714e36ea45f041a0e4682
Emilien Foissotte <emilienfoissotte44@gmail.com>
2025-05-25 10:38:33 +0200

    feat: add sketchybar config

---
+++ .config/sketchybar/install_sketchybar.sh
@@ -0,0 +1,14 @@
+# borrowed from https://github.com/mehd-io/dotfiles
+echo "Installing Dependencies"
+brew install --cask sf-symbols
+brew install jq
+brew install gh
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install --cask font-hack-nerd-font
+
+if [ -d "$HOME/.config/sketchybar" ]; then
+  cp -r $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
+fi
+cp -r sketchybar $HOME/.config/sketchybar
+brew services restart sketchybar
